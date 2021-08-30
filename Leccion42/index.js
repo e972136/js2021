cda395e3fce45aa3e881320c548fe4d47ada3b04
Gaspar <e972136@gmail.com>
2021-08-30 15:56:12 -0600

    desde consola

---
+++ Leccion42/index.js
@@ -0,0 +1,18 @@
+let yo = {
+    nombre : "gaspar",
+    edad : 43,
+    ciudad : "sps"
+}
+//recorrer atributos
+
+for(const xxx in yo)
+{
+    console.log(xxx,yo[xxx]);
+    
+}
+
+const nums = [1,2,3,4];
+
+for(const item of nums){
+    console.log(item);
+}
